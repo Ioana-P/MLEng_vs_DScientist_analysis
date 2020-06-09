1767f07250c0e9e7c3e6a13cbe55681e9f74038f
ioanapreo <ioana.preoteasa@wework.com>
2020-06-09 15:29:08 +0100

    text eda refined; some salary exploration; readme content

--- README.md
+++ README.md
@@ -1,22 +1,50 @@
-# Project Title
+# (WORK IN PROGRESS) Exploring the London Data Science job market
 
-### Brief, 2 sentence description saying why this project is important, e.g.
-##### One of the best ways to solve X / find Y / retrieve Z is to use PCA / TF-IDF / RFs...
+Advice and information about the job market for Data Scienstists and Machine Learning (ML) Engineers abounds on the internet. 
+I wanted to have a look for myself at my own sampled data and see what conclusions I could draw from it.
 
-##### In this project I've done ... 
-##### By the end I achieved ... / found ... / developed ... .
+This project was undertaken for 3 reasons: 
+1. To investigate and test hypotheses about Data Scientists and ML Engineers;
+2. To test presuppositions about the job market for Data Scientists and ML Engineers and
+3. To demonstrate and practice data retrieval via webscraping. 
+
+###### Therefore I retrieved job post data from Indeed.co.uk in late May 2020 using Selenium webdriver and BeautifulSoup. 
+###### I wrangled and analysed the data in Pandas, Seaborn, Matplotlib and NLTK. 
+
+
+##### Here are 3 concrete questions I wanted to answer with this project:
+1. How many job posts for this sector advertise their salary?
+2. Are there any skills that differentiate the two types of job?
+3. How many of the jobs seem to indicate that remote/flexible working is an option?
+
+# Findings in brief (this list is still being expanded):
+
+## AWS and cloud technology
+* from analysing the job descriptions of both job types,the only salient major difference is mentions of AWS (Amazon Web services) are three times as high across ML Eng jobs (>600) than DS jobs (~200);
+* Looking at mentions per job post:
+    * the % ML jobs that mention AWS is:  32.65%
+    * the % DS jobs that mention AWS is:  20.0%
+* Looking at mean number of mentions of AWS:
+    * (Excluding posts that DON'T mention AWS), the mean number of times an ML job post mentions AWS is 2.78
+    * (Excluding posts that DON'T mention AWS), the mean number of times an DS job post mentions AWS is 1.83
+* for the same search but with a broader list of terms (e.g. 'GCP', 'cloud', 'azure'), we get:
+    * Excluding posts that DON'T mention cloud services at all, the mean number of times an ML job post mentions cloud services is 3.39
+    * Excluding posts that DON'T mention cloud services at all, the mean number of times an DS job post mentions cloud services is 2.22
+    
+## Advertising salary:
+The percentage of jobs in our sample that openly state any kind of salary range is 34.37
+    * The percentage of DS jobs in our sample that openly state any kind of salary range is     37.06
+    * The percentage of ML Eng jobs in our sample that openly state any kind of salary range is 32.21
+
+
+    
 _____________________________________________________________________________________________________________________________
-
-###  Significant findings/ Executive Summary
-
-(LINK to youtube video; and/or blog post)
 
 ### The Data at a Glance (EDA)
+![hourly_pay](https://github.com/Ioana-P/MLEng_vs_DScientist_analysis/blob/master/salary_per_hour.jpeg)
 
-_____________________________________________________________________________________________________________________________
 
-
-### Exploration of the text data
+![yearly_salary](https://github.com/Ioana-P/MLEng_vs_DScientist_analysis/blob/master/yearl_salary_dist.jpeg)
 
 
 _____________________________________________________________________________________________________________________________
@@ -24,22 +52,29 @@ ________________________________________________________________________________
 ### Statistical tests comparing salaries between ML Engineers and Data Scientists
 
 
-Limitations of data: small sample size of 390 total (186 for DS; 204 for ML Engineers)
+Limitations of data: small sample size of 421 total (202 for DS; 219 for ML Engineers)
 
-* only from those where 
+* [WIP]
 
 _____________________________________________________________________________________________________________________________
 
 
 ### Insights and possible actions
+[WIP]
+* So far the advice of focusing on SQL is solid advice for either type of job
+* Experience with AWS is helpful with ML Engineering positions, although it's not clear _how_ helpful
+
 
 ### Filing system:
 
 Notebooks
-1. Modelling_and_insights.ipynb - principal notebook of findings and final results; most relevant notebook to most people
-2. Model_building_and_optimization.ipynb - different models were all tried out here, tuned and optimized
-3. EDA.ipynb - exploration of the data; go here for further interesting visuals
-4. data_cleaning.ipynb - notebook detailing the entire cleaning process
+1. Modelling_and_insights.ipynb - empty for the moment
+2. Model_building_and_optimization.ipynb - empty for the moment
+3. EDA.ipynb - exploration of the data, text data primarily, followed by salary data.
+4b. Data_preprocessing.ipynb - preprocessing of the text, extraction of salary and review information was done here
+4a_data_extraction_and_cleaning.ipynb - webscraping and html mining was tested and refined iteratively here
+
+functions.py - refactored code lives here, for eda,cleaning, webscraping.
 
 Folders
 * clean_data - folder containing clean data and any additional summary data generated
--- functions.py
+++ functions.py
@@ -18,46 +18,60 @@ import scipy.cluster.hierarchy as shc
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
+import requests as req
+from dotenv import load_dotenv
+load_dotenv()
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 sns.set_style("darkgrid")
 
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 import nltk
-from sklearn.linear_model import LogisticRegression
-from nltk.probability import FreqDist
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
-from sklearn.metrics import confusion_matrix, multilabel_confusion_matrix
-from sklearn.naive_bayes import MultinomialNB
-from sklearn import metrics
-from sklearn.model_selection import train_test_split, GridSearchCV
-from matplotlib import cm
-import numpy as np
-from sklearn.ensemble import RandomForestClassifier,GradientBoostingClassifier
-from sklearn.metrics import accuracy_score, roc_auc_score, roc_curve
-from sklearn.metrics import f1_score
-from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import silhouette_score
 import string
 
 from nltk.corpus import stopwords
+from nltk import FreqDist
 from nltk.tokenize import RegexpTokenizer
+from nltk.stem import WordNetLemmatizer, PorterStemmer
 import re
 tokenizer = RegexpTokenizer(r'\b\w{3,}\b')
 stop_words = list(set(stopwords.words("english")))
 stop_words += list(string.punctuation)
-# stop_words += ['__', '___']
+import bs4
+from bs4 import BeautifulSoup
+from nltk.util import ngrams
+from nltk.collocations import BigramCollocationFinder
+from nltk.metrics import BigramAssocMeasures
 
-# nltk.download('stopwords')
-# nltk.download('punkt')
-# nltk.download('wordnet')
+import warnings
+import sqlalchemy
+from sqlalchemy import create_engine
 
-
+import wordcloud
 
 
 
 #################################CLEANING#####################################
+def preprocess_data(string):
+    """As a precautionary measure we should try to remove any emails or websites that BS4 missed"""
+    new_str = re.sub(r"\S+@\S+", '', string)
+    new_str = re.sub(r"\S+.co\S+", '', new_str)
+    new_str = re.sub(r"\S+.ed\S+", '', new_str)
+    new_str_tok = tokenizer.tokenize(new_str)
+    new_str_lemm = [lemmy.lemmatize(token) for token in new_str_tok]
+    new_str_cont = ''
+    for tok in new_str_lemm:
+        new_str_cont += tok + ' '
+    return new_str_cont
 
-
+def gen_stopwords(additional_sw = ['data', 'experience', 'learning', 'science', 'machine', 'work', 'company', 'role', 'the', 'skills', ' data', '000', "data", "the", 'join', 'you']):
+    new_stop_words = stop_words
+    for sw in additional_sw:
+        new_stop_words.append(sw)
+    new_stop_words = list(set(new_stop_words))
+    return new_stop_words
 
 
 #################################API-REQUESTS#####################################
@@ -101,14 +115,208 @@ class APICaller:
 #################################SCRAPING#####################################
 
 
+#################################TEXT MINING#####################################
+
+
+def get_salary(dom, parser = 'html', regex_pattern_salary = ['£[0-9]*[,]*[0-9]+[ ]+[a]+'], 
+               regex_pattern_interval=['£[0-9]*[,]*[0-9]+[ ]+[a]*[ ]+year', 
+                                       '£[0-9]*[,]*[0-9]+[ ]+[a]*[ ]+month', 
+                                       '£[0-9]*[,]*[0-9]+[ ]+[a]*[ ]+week',
+                                       '£[0-9]*[,]*[0-9]+[ ]+[a]*[ ]+day',
+                                       '£[0-9]*[,]*[0-9]+[ ]+[an]*[ ]+hour'],
+              ):
+    """Function that should be applied across the elements of a column in a pandas dataframe. 
+    Parses a webpages html code using BeautifulSoup and a specified parser, after which it tries to identify
+    salary mentions using regex. If multiple pattern occurrences are found, the function will return the 
+    mean of the first two, based on the assumption that the web page would feature a string similar to :
+     - Salary range: £25,000 - £32,000
+     Args:
+     dom - block of html code
+     parser - (str) parser that BeautifulSoup should use, e.g. 'html' (default) or 'lxml'
+     regex_pattern_salary - (list of str) lis of patterns for regex to use for retrieving the numerical data
+     regex_pattern_interval - (list of str) list of patterns for regex to use for determining if the salary is
+                            per year, per week, per month.
+     
+     Returns a TUPLE:
+     salaries_final - (int) a single value of salary
+     salary_period - (str) for what time period the salary is declared for: Y - per year; 
+                         M - per month, H - per hour, W - per week, D - per day
+     """
+    dom = str(dom)
+    soup = BeautifulSoup(dom, parser)
+    # BS4 used to parse the dom and just retrieve the text, eliminating any html tags
+    soup_str = soup.get_text()
+    for regex_pattern in regex_pattern_salary:
+        # iterating over the text using the numerical patterns to find salary mentions
+        salary_tokenizer = RegexpTokenizer(regex_pattern)
+        salaries = salary_tokenizer.tokenize(soup_str)
+        # if we get several occurrences, we take the first two mentions only
+        if len(salaries)>1:
+            salaries_lst = [salaries[0], salaries[1]]
+            # removing pound sympol, letters and any commas
+            salaries_clean = [re.sub('£', '', salary) for salary in salaries_lst]
+            salaries_clean = [re.sub('[a-zA-Z]', '', salary) for salary in salaries_clean]
+            salaries_clean = [int(re.sub(',', '', salary)) for salary in salaries_clean]
+            # final salary is the mean of the first two occurrences
+            salaries_final = np.mean([salaries_clean[0], salaries_clean[1]])
+        # if there's only 1 occurence we just take that one
+        elif len(salaries)==1:
+            salary_clean = re.sub('£', '', salaries[0])
+            salary_clean = re.sub('[a-zA-Z]', '', salary_clean)           
+            salary_clean = int(re.sub(',', '', salary_clean))
+            salary_clean = int(salary_clean)
+            salaries_final = salary_clean
+        else:
+            # for ease of dataframe manipulation down the line
+            # posts with no detectable job are left with a NaN
+            salaries_final = np.NaN
+    
+    for regex_pattern in regex_pattern_interval:
+        # now iterating over the regex patterns in the interval list
+        # trying to suss out if the mentioned salary is per year/month/week
+        # break is added after each if partially for efficiency but also 
+        # because of not wanting a latter interval str occurence to 
+        # overwrite the original, e.g. if the post gives a per month salary first then
+        # later repeats in weeks, but the function's stored salary numeric value is the 
+        # one given in months, we want to avoid treating it as if in months
+        interval_tokenizer = RegexpTokenizer(regex_pattern)
+        intervals = interval_tokenizer.tokenize(soup_str)
+        if (type(intervals)==list) and (len(intervals)>1):
+            salary_interval = intervals[0]
+#             print('INTERVAL', salary_interval)
+            break
+        elif type(intervals)==str:
+            salary_interval = intervals
+#             print('INTERVAL', salary_interval)
+            break
+        else:
+            salary_interval = ''
+            continue
+        
+    if (salary_interval!=''):    
+        if 'month' in salary_interval:
+            salaries_final_adjusted = salaries_final/159
+#             print('SALARY PER MONTH is', salaries_final)
+            salary_period = 'M'
+        elif 'week' in salary_interval:
+            salaries_final_adjusted =  salaries_final/36.5
+#             print('SALARY PER MONTH is', salaries_final)
+            salary_period = 'W'
+        elif 'year' in salary_interval:
+            salaries_final_adjusted = salaries_final/1898
+#             print('SALARY PER YEAR is', salaries_final)
+            salary_period = 'Y'
+        elif 'day' in salary_interval:
+            salaries_final_adjusted = salaries_final/7.3
+#             print('SALARY PER DAY')
+            salary_period = 'D'
+        elif 'hour' in salary_interval:
+            salaries_final_adjusted = salaries_final
+#             print('SALARY PER HOUR')
+            salary_period = 'H'
+        else:
+#             print('salary stated for interval that is not comprehended - not reliable long-term data')
+            salaries_final = np.NaN
+            salart_period = np.NaN
+            salaries_final_adjusted = np.NaN
+    else:
+        salary_period = np.NaN
+        salaries_final_adjusted = np.NaN
+    
+    return salaries_final, salaries_final_adjusted, salary_period
+
+
 
 
 
 #################################DATA TRANSFORMATION#####################################
+def get_num_reviews(text, regex_pattern = '[0-9]*[,]*[0-9]* review[s]*'):
+    """Function that should be applied across the elements of a column in a pandas dataframe. 
+    Goes through the text trying to find a pattern for the number of reviews left using regex. 
+    An occurrence of 0 will just return a nan
+     Args:
+     text - (str) input text
+     regex_pattern - (str) pattern for regex to use for retrieving data
+     
+     Returns :
+     num_review - a single
+     """
+    text = str(text)
+    
+    reviews = re.findall(regex_pattern, text)
+    if reviews==[]:
+        num_review_int= np.NaN
+    else:
+        num_review = reviews[-1]
+        num_review_clean = re.sub(',', '', num_review)
+        num_review_int = int(re.sub('review[s]*', '', num_review_clean))
+    return num_review_int
+
 
 
 
 #################################EDA#####################################
+
+def plot_freqdist_from_series(pd_series, tokenizer_obj, stop_words_list, 
+                              title = 'Term Frequency distribution', num_terms=20, 
+                              figsize = (10,10), ngram_number=1, lower_case=True):
+    """Function that takes in a Pandas Series or column of a DataFrame and plots the Frequency Distribution
+    of termns within that list of documents.
+    Args:
+    pd_series - either a standalone Pandas Series object or a dataframe column, e.g. df.job_description
+    tokenizer_obj - (obj) a tokenizer object, normally of the NLTK variety
+    num_terms - (int) how many of the top terms to plot on the Freq Dist, default 20
+    stop_words - (list of str) list of stop words to exclude from final corpus
+    figsize - (tuple of 2 integers) size of matplotlib plot, default is (10,10)
+    ngram_numer - (int) what size ngrams to use, expects 1, 2 or 3. Default is 1.
+                Values outside that list will just return the default. 
+    lower_case - (bool) whether to return all words lowercased or not
+    
+    
+    Plot of the Frequency Distribution of the words in the corpus, using NLTK's built in FreqDist function.
+
+    Returns:
+    f_dist_dict - (dict) unigram / bigrams as keys; frequency as value
+    """
+    all_text_lst = []
+    for string in pd_series.tolist():
+        output_txt = ''
+        tokenized_str = tokenizer_obj.tokenize(string)
+        for word in tokenized_str:
+            if word.lower() not in stop_words_list:
+                if lower_case:
+#                     all_text += word.lower() + ' '   
+                    output_txt += word.lower() + ' '
+                else:
+#                     all_text += word + ' '  
+                    output_txt += word + ' '
+        ngram_list = list(nltk.ngrams(output_txt.split(' ')[:-1], n=ngram_number))
+        for ngram in ngram_list:
+            all_text_lst.append(ngram)
+    
+    f_dist = FreqDist(all_text_lst)
+
+    f_dist_dict = dict(f_dist)
+    
+    plt.figure(figsize=figsize)
+    plt.title(title)
+    f_dist.plot(num_terms)
+    plt.show();
+
+    return f_dist_dict
+
+def gen_cloud(data, max_words_num, 
+              background_color = 'black', height = 300, 
+              randomstate=42, fig_size=(12,12),
+             cloud_title='Word Cloud'):
+    cloud = wordcloud.WordCloud(max_words=max_words_num, background_color=background_color, height=height, random_state=randomstate)
+    plt.figure(figsize=fig_size)
+    cloud.generate(' '.join(data))
+    plt.imshow(cloud)
+    plt.title(cloud_title)
+    plt.axis("off")
+    plt.show();
+
 
 
 
